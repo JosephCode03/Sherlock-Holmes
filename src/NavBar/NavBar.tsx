18bd1184b9167a6e6f029d6e72f68902e19dd61c
Joseph <143991754+JosephCode03@users.noreply.github.com>
2024-09-08 23:31:03 -0700

    routing

--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -6,14 +6,12 @@ export default function NavBar() {
     return(
         <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
-                {/* <a href="#" >Sherlock</a> */}
                 <Link to='/' className="navbar-brand text-info fw-semibold fs-0">Sherlock</Link>
                 
                 {/* Button Menu */}
                 <button className="navbar-toggler" 
                 type="button" data-bs-toggle="offcanvas" data-bs-target="#sideMenu" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation"
                 >
-                  {/* <i className='bx bx-menu'></i> */}
                   <i className="bi bi-list"></i>
                 </button>
 
--- src/Pages/Error404.tsx
+++ src/Pages/Error404.tsx
@@ -1,7 +1,20 @@
+import './Error.css'
+import { Link } from "react-router-dom";
+import imgCat from './image-cat.png'
+
 export default function Error404() {
     return(
-        <div className="container">
-            <h6>Error 404</h6>
+        <div className="container Error">
+            <div className="left text-center">
+                <img src={imgCat} alt="image of a cat"/>
+            </div>
+            <div className="right">
+                <h3>Opps.</h3>
+                <p className='p-1'>We can't find the page you're looking for.</p>
+                <p className='p-2'>It may have expired, or there cloud be a typo. Maybe you can find what you need from pur homepage.</p>
+
+                <Link to='/home' className="btn btn-primary btn-sm">Return to Sherlock.com</Link>
+            </div>
         </div>
     )
 }
